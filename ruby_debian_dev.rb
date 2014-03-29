8d6bfffe81b84f8f3df85860d99ec3abd69f50bf
Antonio Terceiro <terceiro@debian.org>
2014-03-29 14:36:21 -0300

    ruby-all-dev: migrate from Ruby 1.9.1 and 2.0 to Ruby 2.0 and 2.1

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -3,18 +3,18 @@ module RubyDebianDev
   SUPPORTED_RUBY_VERSIONS = {
     #name             Ruby binary
     #---------------  -------------------
-    'ruby1.9.1' => '/usr/bin/ruby1.9.1',
     'ruby2.0'   => '/usr/bin/ruby2.0',
+    'ruby2.1'   => '/usr/bin/ruby2.1',
   }
 
   RUBY_CONFIG_VERSION = {
-    'ruby1.9.1' => '1.9.1',
     'ruby2.0'   => '2.0',
+    'ruby2.1'   => '2.1',
   }
 
   SUPPORTED_RUBY_SHARED_LIBRARIES = [
-    'libruby1.9.1',
     'libruby2.0',
+    'libruby2.1',
   ]
 
 end
