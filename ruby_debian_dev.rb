9b63ebdf5feca44ff383bc88ae79f8a26b4ebb7b
Antonio Terceiro <terceiro@debian.org>
2014-05-06 20:03:03 -0300

    Switch to ruby2.1 and abandon Ruby 2.0

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -3,17 +3,18 @@ module RubyDebianDev
   SUPPORTED_RUBY_VERSIONS = {
     #name             Ruby binary
     #---------------  -------------------
-    'ruby2.0'   => '/usr/bin/ruby2.0',
     'ruby2.1'   => '/usr/bin/ruby2.1',
   }
 
   RUBY_CONFIG_VERSION = {
-    'ruby2.0'   => '2.0',
     'ruby2.1'   => '2.1',
   }
 
+  RUBY_API_VERSION = {
+    'ruby2.1'   => '2.1.0',
+  }
+
   SUPPORTED_RUBY_SHARED_LIBRARIES = [
-    'libruby2.0',
     'libruby2.1',
   ]
 
