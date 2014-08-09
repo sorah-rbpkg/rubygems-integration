71286a25aabe35fcd32a8ef93791445c4ba48557
Antonio Terceiro <terceiro@debian.org>
2014-08-09 18:15:01 -0300

    consolidate interpreter data

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -1,21 +1,32 @@
 module RubyDebianDev
 
-  SUPPORTED_RUBY_VERSIONS = {
-    #name             Ruby binary
-    #---------------  -------------------
-    'ruby2.1'   => '/usr/bin/ruby2.1',
+  RUBY_INTERPRETERS = {
+    'ruby2.1' => {
+      version:             '2.1',
+      binary:              '/usr/bin/ruby2.1',
+      api_version:         '2.1.0',
+      shared_library:      'libruby2.1',
+    },
   }
 
-  RUBY_CONFIG_VERSION = {
-    'ruby2.1'   => '2.1',
-  }
+  #################################################################
+  # These constants below are kept here for backwards compatibility
+  #################################################################
+  SUPPORTED_RUBY_VERSIONS = RUBY_INTERPRETERS.keys.inject({}) do |supported,interpreter|
+    supported[interpreter] = RUBY_INTERPRETERS[interpreter][:binary]
+    supported
+  end
 
-  RUBY_API_VERSION = {
-    'ruby2.1'   => '2.1.0',
-  }
+  RUBY_CONFIG_VERSION = RUBY_INTERPRETERS.keys.inject({}) do |supported,interpreter|
+    supported[interpreter] = RUBY_INTERPRETERS[interpreter][:version]
+    supported
+  end
 
-  SUPPORTED_RUBY_SHARED_LIBRARIES = [
-    'libruby2.1',
-  ]
+  RUBY_API_VERSION = RUBY_INTERPRETERS.keys.inject({}) do |supported,interpreter|
+    supported[interpreter] = RUBY_INTERPRETERS[interpreter][:api_version]
+    supported
+  end
+
+  SUPPORTED_RUBY_SHARED_LIBRARIES = RUBY_INTERPRETERS.map { |k,v| v[:shared_library] }
 
 end
