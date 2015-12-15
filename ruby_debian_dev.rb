802b5497ec413c0719d83ca2d817be60f34c38cd
Antonio Terceiro <terceiro@debian.org>
2015-12-15 21:29:09 -0200

    Add support for building against Ruby 2.3

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -16,6 +16,16 @@ module RubyDebianDev
     }
   end
 
+  has_support_for 'ruby2.3' do
+    {
+      version:             '2.3',
+      binary:              '/usr/bin/ruby2.3',
+      api_version:         '2.3.0',
+      shared_library:      'libruby2.3',
+      min_ruby_dependency: 'ruby (>= 1:2.3~0)',
+    }
+  end
+
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
