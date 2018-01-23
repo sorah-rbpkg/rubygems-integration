c0b81a4213b09d9bc15ae3367c430965476cd6d4
Antonio Terceiro <terceiro@debian.org>
2018-01-23 10:40:35 -0200

    Enable building against ruby2.5

--- ruby_debian_dev.rb
+++ ruby_debian_dev.rb
@@ -16,6 +16,16 @@ module RubyDebianDev
     }
   end
 
+  has_support_for 'ruby2.5' do
+    {
+      version:             '2.5',
+      binary:              '/usr/bin/ruby2.5',
+      api_version:         '2.5.0',
+      shared_library:      'libruby2.5',
+      min_ruby_dependency: 'ruby (>= 1:2.5~0)',
+    }
+  end
+
   def self.min_ruby_dependency_for(shared_library)
     RUBY_INTERPRETERS.each do |int,data|
       if data[:shared_library] == shared_library
