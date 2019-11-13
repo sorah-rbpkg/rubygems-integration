0290aa5b3b2e7d1c07d91a4c8e1e971903903d57
Antonio Terceiro <terceiro@debian.org>
2019-11-13 16:51:45 -0300

    Replace usage of Gem::ConfigMap with RbConfig::CONFIG
    Gem::ConfigMap is deprecated in ruby 2.7

--- lib/rubygems/defaults/operating_system.rb
+++ lib/rubygems/defaults/operating_system.rb
@@ -7,7 +7,7 @@ class << Gem
 
   alias :upstream_default_dir :default_dir
   def default_dir
-    File.join('/', 'var', 'lib', 'gems', Gem::ConfigMap[:ruby_version])
+    File.join('/', 'var', 'lib', 'gems', RbConfig::CONFIG["ruby_version"])
   end
 
   alias :upstream_default_bindir :default_bindir
@@ -18,8 +18,8 @@ class << Gem
   alias :upstream_default_path :default_path
   def default_path
 
-    arch = Gem::ConfigMap[:arch]
-    api_version = Gem::ConfigMap[:ruby_version]
+    arch = RbConfig::CONFIG["arch"]
+    api_version = RbConfig::CONFIG["ruby_version"]
 
     upstream_default_path + [
       File.join('/usr/share/rubygems-integration', api_version),
