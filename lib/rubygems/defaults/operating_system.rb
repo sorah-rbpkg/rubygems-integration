9f735958661373c3263ffe17cbbbb607377a5f2e
Sorah Fukumori <sorah@cookpad.com>
2019-12-25 21:47:31 +0900

    rubocop

--- lib/rubygems/defaults/operating_system.rb
+++ lib/rubygems/defaults/operating_system.rb
@@ -1,43 +1,44 @@
+# frozen_string_literal: true
+
 unless ENV['DEBIAN_DISABLE_RUBYGEMS_INTEGRATION']
 
   class << Gem
     OPERATING_SYSTEM_DEFAULTS = {
-      :ssl_ca_cert => '/etc/ssl/certs/ca-certificates.crt'
-    }
+      ssl_ca_cert: '/etc/ssl/certs/ca-certificates.crt'
+    }.freeze
 
-    alias :upstream_default_dir :default_dir
+    alias upstream_default_dir default_dir
     def default_dir
-      File.join('/', 'var', 'lib', 'gems', RbConfig::CONFIG["ruby_version"])
+      File.join('/', 'var', 'lib', 'gems', RbConfig::CONFIG['ruby_version'])
     end
 
-    alias :upstream_default_bindir :default_bindir
+    alias upstream_default_bindir default_bindir
     def default_bindir
       File.join('/', 'usr', 'local', 'bin')
     end
 
-    alias :upstream_default_path :default_path
+    alias upstream_default_path default_path
     def default_path
-
-      arch = RbConfig::CONFIG["arch"]
-      api_version = RbConfig::CONFIG["ruby_version"]
+      arch = RbConfig::CONFIG['arch']
+      api_version = RbConfig::CONFIG['ruby_version']
 
       upstream_default_path + [
         "/usr/lib/ruby/gems/#{api_version}",
         File.join('/usr/share/rubygems-integration', api_version),
         '/usr/share/rubygems-integration/all',
         upstream_default_dir,
-        "/usr/lib/#{arch}/rubygems-integration/#{api_version}",
+        "/usr/lib/#{arch}/rubygems-integration/#{api_version}"
       ].compact
     end
 
     def default_specifications_dir
-      File.join(Gem.upstream_default_dir, "specifications", "default")
+      File.join(Gem.upstream_default_dir, 'specifications', 'default')
     end
   end
 
-  if RUBY_VERSION >= '2.1' and RUBY_VERSION < '2.7' then
+  if (RUBY_VERSION >= '2.1') && (RUBY_VERSION < '2.7')
     class << Gem::BasicSpecification
-      alias :upstream_default_specifications_dir :default_specifications_dir
+      alias upstream_default_specifications_dir default_specifications_dir
       def default_specifications_dir
         Gem.default_specifications_dir
       end
